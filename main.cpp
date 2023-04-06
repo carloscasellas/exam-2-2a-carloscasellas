56831806f44d0109ec17d8c4e77bdaec65bd5299
carloscasellas <carloscg10@yahoo.com>
2023-04-06 18:41:31 +0000

    Output looks correct

--- main.cpp
+++ main.cpp
@@ -10,11 +10,22 @@ int main()
 
     start1 = chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count();
     // ##### START OF SECTION 1 #####
-    // create a vector of n threads
+    vector<thread> v;
     
     // create n threads
 
+    for(int j = 0; j < nthreads; ++j) {
+
+        v.push_back(thread(single_sum_thread, j, nthreads, sum));
+
+    }
+
     // join the threads with the main thread
+    for(int j = 0; j < nthreads; ++j) {
+
+        v[j].join();
+
+    }
     
     // ##### END OF SECTION 1 #####
     end1 = chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count();
@@ -24,11 +35,28 @@ int main()
     // parallelize the following code, reference main.h for the function prototype
     // comment out this block of code when you are done with the parallelization 
     // (only need sleep 1 in the thread function)
-    for (i = 0, pi = 0.0; i < nthreads; i++)
-    {
-        pi += sum[i][0] * step;
-        sleep(1); // simulate a long running task
+
+    vector<thread> v2;
+
+    for(i = 0; i < nthreads; ++i) {
+
+        v2.push_back(thread(pi_sum_thread, sum, &pi, i));
+
     }
+
+    for(i = 0; i < nthreads; ++i) {
+
+        v2[i].join();
+
+    }
+
+
+
+    // for (i = 0, pi = 0.0; i < nthreads; i++)
+    // {
+    //     pi += sum[i][0] * step;
+    //     sleep(1); // simulate a long running task
+    // }
 
     // ##### END OF SECTION 2 #####
     end2 = chrono::duration_cast<chrono::nanoseconds>(chrono::high_resolution_clock::now().time_since_epoch()).count();
